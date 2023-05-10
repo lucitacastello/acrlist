aae434d2205665e4120a590cbcf6fd8f89c6d0cf
Cida Castello <56411540+cidacastello@users.noreply.github.com>
2023-05-10 15:27:18 -0300

    Project created

---
+++ src/main/java/com/acrdev/acrlist/AcrlistApplication.java
@@ -0,0 +1,13 @@
+package com.acrdev.acrlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcrlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcrlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acrdev/acrlist/AcrlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acrdev.acrlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcrlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
